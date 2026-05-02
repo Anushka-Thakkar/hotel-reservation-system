7f19748d1febb0ba9b4ee60ffff89bf19c2f1474
Anushka <appleuser@Anushkas-MacBook-Air-2.local>
2026-05-02 14:25:19 +0530

    Final update: secure JDBC code using environment variables

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password"; //enter your root password
-
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
     public static void main(String[] args) {
 
         try (
